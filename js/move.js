71397dc8c9ac2d65afbbc31a7666c37f3d4c2260
Túlio <edd.di@unip.br>
2021-05-06 01:39:47 -0300

    mais uma tentativa de tentar consertar o bug de movimento

--- js/move.js
+++ js/move.js
@@ -1,158 +1,191 @@
-
-var updateL;
-var updateR;
-var updateMove;
-var side;
-var posX = 0;
+var side = 'idle';
 var move = false;
-
-
-
-let widthScreen = document.body.clientWidth;
-
-
-
-function turnShip() {
-  console.log('ta rodando');
-  if(side == "R" &&  move == true && posX < (widthScreen / 2 - 50)){
-    var newPosR = posX += 10;
-    ship.style.transform = "translateX(" + newPosR + "px)";
-
-  }
-  else if(side == "L" && move == true && posX > (widthScreen / 2 - 50) * -1){
-    var newPosL = posX += -10;
-    ship.style.transform = "translateX(" + newPosL + "px)";
-
-  }else{
-    // updateMove = '';
-    clearInterval(updateMove);
-
-  }
-}
-
-// function turnR() {
-
-//   if (posX < widthScreen / 2 - 50 && move === true) {
-//     var newPosR = posX += 7;
-//     ship.style.transform = "translateX(" + newPosR + "px)";
-//   }
-
-// }
-
-
-// function turnL() {
-
-//   if (posX > (widthScreen / 2 - 50) * -1 && move === true) {
-//     var newPosL = posX += -7;
-//     ship.style.transform = "translateX(" + newPosL + "px)";
-//   }
-
-// }
+var moving;
+var posX = 0;
 
 document.addEventListener("keydown", event => {
-  // console.log(event);
-  // ------------ Right -------------------------------
-  if (event.key === 'ArrowRight' && event.repeat === false && !ship.classList.contains('turn-L')) {
 
-    
-    ship.classList.add("turn-R");
+  if(event.isTrusted == false){
+    clearTimeout(moving);
+  }
+
+  if (event.key === 'ArrowRight' && event.key === 'ArrowLeft') {
+    clearTimeout(moving)
+  }
+
+  if (event.key === 'ArrowRight' && event.repeat == false && side !== 'L') {
+
     console.log('R-down');
-    // clearInterval(updateMove);
+    ship.classList.add("turn-R");
+    side = 'R';
     move = true;
-    side = "R";
-    updateMove = setInterval(turnShip, 20);
-    
+    moveShip();
 
   }
 
-  // ------------ Left -------------------------------
-  if (event.key === 'ArrowLeft' && event.repeat === false && !ship.classList.contains('turn-R')) {
-
-    ship.classList.add("turn-L");
+  if (event.key === 'ArrowLeft' && event.repeat == false && side !== 'R') {
     console.log('L-down');
+    ship.classList.add("turn-L");
+    side = 'L';
     move = true;
-    side = "L";
-    updateMove = setInterval(turnShip, 20);
-
-
+    moveShip();
   }
-
-  // if (event.key === 'ArrowRight' && event.key === 'ArrowRight') {
-  //   clearInterval(updateMove);
-  // }
-
-
-  // ------------ Right -------------------------------
-  // if (event.which === 39 && event.repeat === false && !ship.classList.contains('turn-L')) {
-
-
-  //   ship.classList.add("turn-R");
-  //   console.log('R-down');
-  //   clearInterval(updateL);
-  //   move = true;
-  //   updateR = setInterval(turnR, 10);
-
-  // }
-
-  // ------------ Left -------------------------------
-  // if (event.keyCode === 37 && event.repeat === false && !ship.classList.contains('turn-R')) {
-
-  //   ship.classList.add("turn-L");
-  //   console.log('L-down');
-  //   clearInterval(updateR);
-  //   move = true;
-  //   updateL = setInterval(turnL, 10);
-
-  // }
-
 
 
 });
-
-
-
 
 document.addEventListener("keyup", event => {
 
-  if (event.key === 'ArrowRight' && event.key === 'ArrowRight') {
-    clearInterval(updateMove);
-  }
 
-  // -------- Right ------------
+
   if (event.key === 'ArrowRight') {
-    move = false;
+
+    console.log('R-up');
     ship.classList.remove("turn-R");
-    clearInterval(updateMove);
-
-  }
-
-  // -------- Left ------------
-  if (event.key === 'ArrowLeft') {
+    side = 'idle';
     move = false;
-    ship.classList.remove("turn-L");
-    clearInterval(updateMove);
+    clearTimeout(moving);
+
   }
 
+  if (event.key === 'ArrowLeft') {
 
-
-  // // -------- Right ------------
-  // if (event.which === 39) {
-
-  //   ship.classList.remove("turn-R");
-  //   console.log('R-up');
-  //   move = false;
-  //   clearInterval(updateL);
-  //   clearInterval(updateR);
-  // }
-
-  // // -------- Left ------------
-  // if (event.keyCode === 37) {
-
-  //   ship.classList.remove("turn-L");
-  //   console.log('L-up');
-  //   move = false;
-  //   clearInterval(updateL);
-  //   clearInterval(updateR);
-  // }
+    console.log('L-up');
+    ship.classList.remove("turn-L");
+    side = 'idle';
+    move = false;
+    clearTimeout(moving);
+  }
 
 });
+
+
+function moveShip() {
+
+  
+  let widthScreen = document.body.clientWidth;
+
+  if (side == 'R' && move == true && posX < (widthScreen / 2 - 50)) {
+    console.log('turning-R');
+    moving = setTimeout(moveShip, 20); 7
+
+    var newPosR = posX += 10;
+    ship.style.transform = "translateX(" + newPosR + "px)";
+  }else{
+    ship.classList.remove("turn-R");
+    
+  }
+
+  if (side == 'L' && move == true && posX > (widthScreen / 2 - 50) * -1) {
+    console.log('turning-L');
+    moving = setTimeout(moveShip, 20);
+
+    var newPosL = posX += -10;
+    ship.style.transform = "translateX(" + newPosL + "px)";
+  }else{
+    ship.classList.remove("turn-L");
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var updateL;
+// var updateR;
+// var updateMove;
+// var side;
+// var posX = 0;
+// var move = false;
+
+
+
+// let widthScreen = document.body.clientWidth;
+
+
+
+// function turnShip() {
+//   console.log('ta rodando');
+//   if(side == "R" &&  move == true && posX < (widthScreen / 2 - 50)){
+//     var newPosR = posX += 10;
+//     ship.style.transform = "translateX(" + newPosR + "px)";
+
+//   }
+//   else if(side == "L" && move == true && posX > (widthScreen / 2 - 50) * -1){
+//     var newPosL = posX += -10;
+//     ship.style.transform = "translateX(" + newPosL + "px)";
+
+//   }else{
+//     clearInterval(updateMove);
+
+//   }
+// }
+
+
+// document.addEventListener("keydown", event => {
+
+//   // ------------ Right -------------------------------
+//   if (event.key === 'ArrowRight' && event.repeat === false && !ship.classList.contains('turn-L')) {
+
+
+//     ship.classList.add("turn-R");
+//     console.log('R-down');
+//     move = true;
+//     side = "R";
+//     updateMove = setInterval(turnShip, 20);
+
+
+//   }
+
+//   // ------------ Left -------------------------------
+//   if (event.key === 'ArrowLeft' && event.repeat === false && !ship.classList.contains('turn-R')) {
+
+//     ship.classList.add("turn-L");
+//     console.log('L-down');
+//     move = true;
+//     side = "L";
+//     updateMove = setInterval(turnShip, 20);
+
+
+//   }
+
+// });
+
+
+
+
+// document.addEventListener("keyup", event => {
+
+//   if (event.key === 'ArrowRight' && event.key === 'ArrowRight') {
+//     clearInterval(updateMove);
+//   }
+
+//   // -------- Right ------------
+//   if (event.key === 'ArrowRight') {
+//     move = false;
+//     ship.classList.remove("turn-R");
+//     clearInterval(updateMove);
+
+//   }
+
+//   // -------- Left ------------
+//   if (event.key === 'ArrowLeft') {
+//     move = false;
+//     ship.classList.remove("turn-L");
+//     clearInterval(updateMove);
+//   }
+
+// });
